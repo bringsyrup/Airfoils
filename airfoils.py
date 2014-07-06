7a719a7413189d0004468cfe9f544e321baf2c4c
Ruby Spring <rubymspring@gmail.com>
2014-07-06 16:55:16 -0400

    works as independent class now, more options for different data types

--- airfoils.py
+++ airfoils.py
@@ -13,11 +13,15 @@ class airfoil(object):
     def __init__(self, data_in):
         self.data_in = data_in
 
-    def scale(self, x):
+    def scale(self, x, opt=False):
         with open(self.data_in) as input_file:
             data_lists = []
             for line in input_file:
                 data_lists.append(line.strip().split())
+            if opt == True:
+                chord_pair = data_lists[0]
+                chord = float(chord_pair[0])
+                x = x / chord
         scaled_lists = []
         for lst in data_lists:
             scaled_list = []
@@ -27,10 +31,10 @@ class airfoil(object):
             scaled_lists.append(scaled_list)
         return scaled_lists    
 
-    def write_new(self, x=1.0):
+    def write_new(self, x=1.0, opt=False):
         filename = self.data_in[:-4] + "_S%s.txt" % x
         new_file = open(filename, 'w')
-        scaled_lists = self.scale(x)
+        scaled_lists = self.scale(x, opt)
         for lst in scaled_lists:
             string_pair = ""
             for i in lst:
@@ -43,8 +47,8 @@ class airfoil(object):
             new_file.write(string_pair + '\n')
         new_file.close()
 
-    def fix_data(self, n, x=1.0):
-        scaled_lists = self.scale(x)
+    def fix_data(self, n, x=1.0, opt=False):
+        scaled_lists = self.scale(x, opt)
         x_list = []
         y_list = []
         for pair in scaled_lists:
@@ -52,13 +56,11 @@ class airfoil(object):
             y_list.append(pair[1])
         x_array = np.asarray(x_list)
         y_array = np.asarray(y_list)
-        if fix >= 1:
+        if n >= 1:
             #this option creates shitty data at the moment. working on it!
             for i in range(len(x_array)):
                 if x_array[i] == y_array[i]:
                     stop = i
-                    print stop
-                    print x_array[i]
                     break
             px_array = x_array[:stop+1]
             py_array = y_array[:stop+1]
@@ -72,16 +74,14 @@ class airfoil(object):
             py_fixed = py_poly(px_fixed)
             nx_fixed = np.linspace(min(nx_array), max(nx_array), 100)
             ny_fixed = ny_poly(nx_fixed)
-            print nx_fixed
-            print ny_fixed
             x_fixed = np.concatenate([px_fixed, nx_fixed])
             y_fixed = np.concatenate([py_fixed, ny_fixed])
             return x_fixed, y_fixed
         else:
             return x_array, y_array
 
-    def plot_data(self, n, x=1.0):
-        x_array, y_array = self.fix_data(n, x)
+    def plot_data(self, n, x=1.0, opt=False):
+        x_array, y_array = self.fix_data(n, x, opt)
         plot(x_array, y_array, linestyle="-")
         xlabel("x coordinates, any units")
         ylabel("y coordinates, any units")
@@ -102,11 +102,7 @@ if __name__=="__main__":
             type = str,
             help = "must be .txt format containing x and y data columns. if data contains third column for z coordinates, it will be ignored and replaced with zeros. any whitespace between data points is used to split the data and eliminated"
             )
-    parser.add_argument("-f", "--fix",
-            action = "store",
-            type = int,
-            help = "attempts to fix data with too few data points with a polyfit"
-            )
+    
     parser.add_argument("-w", "--write",
             action = "store_true",
             help = "writes manipulated data to new file with automated filename. overwrites existing filename if it exists already. see README.md for examples"
@@ -116,14 +112,27 @@ if __name__=="__main__":
             type = float,
             help = "float type, scales the data to data * s"
             )
-    
+    parser.add_argument("-o", "--optimal",
+            action = "store_true",
+            help = "default False, True if selected. turns --scale argument into the maximum chord length"
+            )
     parser.add_argument("-p","--plot",
             action = "store_true",
             help = "plots the scaled data. if data_out given, new file will still be written"
             )
+    parser.add_argument("-f", "--fix",
+            action = "store",
+            type = int,
+            help = "attempts to fix data with too few data points with a polyfit"
+            )
     args = parser.parse_args()
     
     new_airfoil = airfoil(args.data_in)
+
+    if args.optimal:
+        opt_cond = True
+    else:
+        opt_cond = False
 
     if args.fix:
         fix = args.fix
@@ -132,17 +141,18 @@ if __name__=="__main__":
     
     if args.write:
         if args.scale and args.plot:
-            new_airfoil.write_new(args.scale)
-            new_airfoil.plot_data(fix, args.scale)
+            new_airfoil.write_new(args.scale, opt_cond)
+            new_airfoil.plot_data(fix, args.scale, opt_cond)
+        #elif args.optimal and args.plot:
         elif args.scale:
-            new_airfoil.write_new(args.scale)
+            new_airfoil.write_new(args.scale, opt_cond)
         elif args.plot:
             new_airfoil.plot_data(fix)
         else:
             new_airfoil.write_new()
     elif args.plot:
         if args.scale:
-            new_airfoil.plot_data(fix, args.scale)
+            new_airfoil.plot_data(fix, args.scale, opt_cond)
         else:
             new_airfoil.plot_data(fix)
     else:
